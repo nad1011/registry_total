e796b364270b60adbdf15f3511ccd16f15b6b5e8
Haidan1011 <101380985+Haidan1011@users.noreply.github.com>
2023-04-23 17:16:34 +0700

    Change UI of Shop.jsx and add web icon + web name

--- src/components/RenewForm/RenewForm.jsx
+++ src/components/RenewForm/RenewForm.jsx
@@ -2,10 +2,11 @@ import { useState, useContext, useEffect } from "react";
 import * as FiIcon from "react-icons/fi";
 import * as BsIcon from "react-icons/bs";
 import FormInput from "../FormInput/FormInput";
-import Box from '@mui/material/Box';
+import Box from "@mui/material/Box";
 import { FormContext } from "../../contexts/FormContext";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 import styles from "./RenewForm.module.css";
+import Stack from "@mui/material/Stack";
 
 const RenewForm = ({ car }) => {
   const { autoName, autoNumberPlate } = useContext(FormContext);
@@ -57,58 +58,81 @@ const RenewForm = ({ car }) => {
   }, [autoName]);
 
   return (
-    <Box sx={{width:1 , p: 2, border: '2px solid black', borderRadius: 10 }} className={styles['container']}>
-      <h2 className={styles["form-header"]}>Renew Car Registration</h2>
-      <form onSubmit={submitHandle}>
-        <FormInput
-          label="Name"
-          type="text"
-          required
-          onChange={handleChange}
-          name="name"
-          value={name}
-          icon=<FiIcon.FiUser />
-        />
-        <FormInput
-          label="Number Plate"
-          type="text"
-          required
-          onChange={handleChange}
-          name="numberPlate"
-          value={numberPlate}
-          icon=<BsIcon.BsCarFront />
-        />
-        <FormInput
-          label="New expiration date"
-          type="text"
-          required
-          onChange={handleChange}
-          name="newExpirationDate"
-          value={newExpirationDate}
-          icon=<BsIcon.BsCalendarHeart />
-        />
-        <div className={styles["checkbox-container"]}>
-          <input
-            type="checkbox"
-            name="fee-check"
-            checked={feeCheck}
-            value={feeCheck}
+    <Box
+      sx={{
+        width: 1,
+        height: 630,
+        p: 1.5,
+        border: "2px solid var(--secondary-color)",
+        bgcolor: "var(--secondary-color)",
+        borderRadius: 3,
+      }}
+      className={styles["container"]}
+    >
+      <Stack
+        direction="column"
+        justifyContent="space-between"
+        alignItems="center"
+        height={1}
+      >
+        <Box width={0.9} sx={{ mt: 2 }}>
+          <h2 className={styles["form-header"]}>Renew Car Registration</h2>
+        </Box>
+        <form onSubmit={submitHandle}>
+          <FormInput
+            label="Name"
+            type="text"
             required
-            onChange={handleFeeCheck}
+            onChange={handleChange}
+            name="name"
+            value={name}
+            icon=<FiIcon.FiUser />
           />
-          <label htmlFor="fee-check">Đóng đủ phí</label>
-          <input
-            type="checkbox"
-            name="car-check"
-            checked={carCheck}
-            value={carCheck}
+          <FormInput
+            label="Number Plate"
+            type="text"
             required
-            onChange={handleCarCheck}
+            onChange={handleChange}
+            name="numberPlate"
+            value={numberPlate}
+            icon=<BsIcon.BsCarFront />
           />
-          <label htmlFor="car-check">Đã kiểm tra xe</label>
-        </div>
-        <Button variant="contained">Confirm</Button>
-      </form>
+          <FormInput
+            label="New expiration date"
+            type="text"
+            required
+            onChange={handleChange}
+            name="newExpirationDate"
+            value={newExpirationDate}
+            icon=<BsIcon.BsCalendarHeart />
+          />
+          <div className={styles["checkbox-container"]}>
+            <input
+              type="checkbox"
+              name="fee-check"
+              checked={feeCheck}
+              value={feeCheck}
+              required
+              onChange={handleFeeCheck}
+            />
+            <label htmlFor="fee-check">Đóng đủ phí</label>
+            <input
+              type="checkbox"
+              name="car-check"
+              checked={carCheck}
+              value={carCheck}
+              required
+              onChange={handleCarCheck}
+            />
+            <label htmlFor="car-check">Đã kiểm tra xe</label>
+          </div>
+        </form>
+        <Button
+          sx={{ backgroundColor: "whitesmoke", color: "var(--avatar-color)", fontWeight: "bold" }}
+          size="lg"
+          variant="solid"
+        >Confirm</Button>
+      </Stack>
     </Box>
   );
 };

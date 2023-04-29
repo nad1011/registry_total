2aad4d0366aa5f39ab389bb9522afe7f3c5cc013
Haidan1011 <101380985+Haidan1011@users.noreply.github.com>
2023-04-29 13:38:14 +0700

    Co-authored-by: Nguyễn Thành Dũng <LostArrows27@users.noreply.github.com>

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <div>Home</div>;
+import React, { useState } from 'react';
+import ToggleSwitch from '../../components/TripleToggleSwitch/TripleToggleSwitch';
+import Page from '../Page/Page';
+
+function App() {
+  const [selectedValue, setSelectedValue] = useState('Tháng');
+  
+  const handleToggle = (value) => {
+    setSelectedValue(value);
+  }
+  
+  return (
+    <Page>
+      <h1>Hello World!</h1>
+      <ToggleSwitch 
+        values={['Tháng', 'Quý', 'Năm']} 
+        selected={selectedValue} 
+        onChange={handleToggle}
+      />
+      <p>You have selected: {selectedValue}</p>
+    </Page>
+  );
 }
+
+export default App;
